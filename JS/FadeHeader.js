bd365dec3e7ae75dcd509d39cac2da51bb2f9ba9
BaraldsBR <baraldsbr@gmail.com>
2022-06-07 13:26:44 -0300

    Início + Header

---
+++ JS/FadeHeader.js
@@ -0,0 +1,15 @@
+function ChangeOpacity()
+{
+    header = document.getElementById("header");
+    console.log(scrollY);
+    if(scrollY <= 150) 
+    {
+        header.style.backgroundColor = "rgba(48, 51, 51, " + (scrollY*255/150).toString() + ")";
+    }
+    else 
+    {
+        header.style.backgroundColor = "rgba(48, 51, 51, 255)";
+    }
+}
+
+setInterval(() => {ChangeOpacity()}, 50);
